71be2ced425377771d06e12938f700c7d2e6abbc
Corey Lutrick <coreylutrick@gmail.com>
2018-02-26 20:31:48 -0800

    did the second challenge

--- strings.js
+++ strings.js
@@ -1,3 +1,7 @@
 //Challenge One
 var numOfSandwiches = 4*2;
 console.log ("numOfSandwiches", numOfSandwiches);
+
+var name = "Corey"
+var string = "Hello, " + name + " how are you doing today?"
+console.log("string " + string);
