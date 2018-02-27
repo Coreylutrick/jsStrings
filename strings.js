af4d7cbcc048a6f6e11191647f65f0d7b3c59ab6
Corey Lutrick <coreylutrick@gmail.com>
2018-02-26 20:41:36 -0800

    finished challenge three

--- strings.js
+++ strings.js
@@ -1,7 +1,11 @@
 //Challenge One
 var numOfSandwiches = 4*2;
 console.log ("numOfSandwiches", numOfSandwiches);
-
+//Challenge Two
 var name = "Corey"
 var string = "Hello, " + name + " how are you doing today?"
-console.log("string " + string);
+console.log("string ", string);
+//Challenge Three
+var DNA = "GCAT"
+var RNA = DNA.replace(/T/g, "U")
+console.log("RNA", RNA);
