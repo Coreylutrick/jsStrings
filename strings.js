f0c42418c668c30fedaf4ac4c275434fa026ce69
Corey Lutrick <coreylutrick@gmail.com>
2018-02-26 21:09:05 -0800

    finished challenge 4

--- strings.js
+++ strings.js
@@ -1,11 +1,26 @@
 //Challenge One
 var numOfSandwiches = 4*2;
 console.log ("numOfSandwiches", numOfSandwiches);
+
 //Challenge Two
 var name = "Corey"
 var string = "Hello, " + name + " how are you doing today?"
 console.log("string ", string);
+
 //Challenge Three
 var DNA = "GCAT"
 var RNA = DNA.replace(/T/g, "U")
 console.log("RNA", RNA);
+
+//Challenge Four
+//var animal = "rabbit";
+//var animal = "alligator";
+//var animal = "turtle";
+var animal = "ALligAtOR";
+//var animal = "Rhino";
+
+if (animal.toLowerCase()  === "alligator"){
+    console.log("small");
+ } else {
+    console.log("wide");
+}
